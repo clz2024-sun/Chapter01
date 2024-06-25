b9ab5c0416c99008f48a76300ad6ba826b8c5359
clz2024.sun <clz2024.sun@gmail.com>
2024-06-25 19:08:29 +0900

    1st

--- src/com/javaex/ex03/Ex09.java
+++ src/com/javaex/ex03/Ex09.java
@@ -4,6 +4,14 @@ import java.util.Scanner;
 
 public class Ex09 {
 
+/*
+	if ~ else 문에서 모든 조건이 == 이면
+	switch ~ case 문으로 표현할 수 있다
+	
+	아래는 if ~ else 문으로 표현
+*/
+	
+	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.print("과목번호:");
--- src/com/javaex/ex03/Ex12.java
+++ src/com/javaex/ex03/Ex12.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Ex12 {
 
 	// 이렇게 줄여서 짤수 있으나 
-	// 많은 생각을 해야 해석할 수 있는 코드는 덜 좋은 코드이다
+	// 많은 생각을 해야 해석할 수 있는 코드는 "덜 좋은 코드"이다
 	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
